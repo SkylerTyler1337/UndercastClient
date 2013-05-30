6b887c159e65ae3cf13d444e6aa442fcf3a9b22a
palechip <palechip@gmail.com>
2013-05-30 13:24:17 +0200

    ServerCommandParser rewrite

--- undercast/client/internetTools/ServersCommandParser.java
+++ undercast/client/internetTools/ServersCommandParser.java
@@ -14,39 +14,55 @@ import undercast.client.UndercastData.MatchState;
 public class ServersCommandParser {
     private static boolean isListening = false;
     //is set to true if the mod casts /servers in order to delete the messages
-    public static boolean castedByMod = false;
-    public static boolean nextCastedByMod = false;
-    public static int pages = 0;
+    private static boolean castedByMod = false;
+    private static boolean nextCastedByMod = false;
+    
+    private static int pages = 0;
+    private static int currentPage = 0;
+    
+    private static final String PAGE_TITLE = "--------- Overcast Network Servers";
+    private static final String FIRST_PAGE_TITLE = "--------- Overcast Network Servers (1";
+    private static final String MESSAGE_CHARACTERISTIC = "Online: "; // string which all non title messges have in comon
+    private static final String MAP_SEPARATOR = "Current Map: ";
+    private static final String MAP_SEPARATOR_UNSTRIPED = "Current Map§f: ";
     
     public static void handleChatMessage(String message, String unstripedMessage) {
-        System.out.println("L: " + isListening + " M: " + castedByMod + " N: " + nextCastedByMod);
-        if(isListening) {
+        if (isListening) {
             // check if the message belongs to the command
             boolean commandEnded = false;
-            if ((!message.contains("Online: ") && !message.contains("--------- Overcast Network Servers")) || message.contains("--------- Overcast Network Servers (1")) {
+            if ((!message.contains(MESSAGE_CHARACTERISTIC) && !message.contains(PAGE_TITLE)) || message.contains(FIRST_PAGE_TITLE)) {
                 isListening = false;
-                castedByMod = nextCastedByMod;
-                nextCastedByMod = false;
-                UndercastCustomMethods.sortAndFilterServers();
+                if(currentPage == pages) {
+                    castedByMod = nextCastedByMod;
+                    nextCastedByMod = false;
+                    UndercastCustomMethods.sortAndFilterServers();
+                }
                 commandEnded = true;
             }
 
             // don't try to handle it if the command ended
-            if(!commandEnded) {
-                //our only interest is the MatchState
-                if(message.contains("Current Map: ")) {
+            if (!commandEnded) {
+                if (message.contains(PAGE_TITLE)) {
+                    try {
+                        currentPage = Integer.parseInt(message.substring(message.indexOf(" of ") - 1, message.indexOf(" of ")));
+                    } catch (Exception e) {
+                        currentPage = 1;
+                    }
+                }
+                // our only interest is the MatchState
+                if (message.contains(MAP_SEPARATOR)) {
                     String name;
                     String map;
                     char matchStatusColor;
                     MatchState state;
 
                     name = message.substring(0, message.indexOf(": "));
-                    map = message.substring(message.indexOf("Current Map: ") + 13);
-                    matchStatusColor = unstripedMessage.charAt(unstripedMessage.indexOf("Current Map§f: ") + 16);
+                    map = message.substring(message.indexOf(MAP_SEPARATOR) + 13);
+                    matchStatusColor = unstripedMessage.charAt(unstripedMessage.indexOf(MAP_SEPARATOR_UNSTRIPED) + 16);
 
-                    //c == red
-                    //e == yellow
-                    //a = green
+                    // c == red
+                    // e == yellow
+                    // a = green
                     switch(matchStatusColor) {
                     case 'a':
                         state = MatchState.Starting;
@@ -61,8 +77,8 @@ public class ServersCommandParser {
                         state = MatchState.Unknown;
                     }
 
-                    //insert the data
-                    for(int c = 0; c < UndercastData.serverInformation.length; c++) {
+                    // insert the data
+                    for (int c = 0; c < UndercastData.serverInformation.length; c++) {
                         if(!(UndercastData.serverInformation[c].name == null)) {
                             if(UndercastData.serverInformation[c].name.equals(name)) {
                                 UndercastData.serverInformation[c].currentMap = map;
@@ -71,31 +87,11 @@ public class ServersCommandParser {
                         }
                     }
                 }
-                // remove the message
-                if(castedByMod) {
-                    try {
-                        List chatLines;
-                        // get the lines
-                        chatLines = (List)ModLoader.getPrivateValue(GuiNewChat.class, Minecraft.getMinecraft().ingameGUI.getChatGUI(), 3);
-                        //remove the message (20 most recent chat messages are enough)
-                        for(int c = 0; c < 20; c++) {
-                            ChatLine line = (ChatLine)chatLines.get(c);
-                            if(StringUtils.stripControlCodes(line.getChatLineString()).contains("Online: ") || StringUtils.stripControlCodes(line.getChatLineString()).contains("--------- Overcast Network Servers")) {
-                                chatLines.remove(c);
-                                break;
-                            }
-                        }
-                        // set them back
-                        ModLoader.setPrivateValue(GuiNewChat.class, Minecraft.getMinecraft().ingameGUI.getChatGUI(), 3, chatLines);
-                    } catch (Exception e) {
-                        System.out.println("[UndercastMod]: Getting a private value (chatLines) failed");
-                        System.out.println("[UndercastMod]: ERROR: " + e.toString());
-                    }
-                }
+                removeChatMessage();
             }
         }
 
-        if(!isListening && message.contains("--------- Overcast Network Servers (1 of ") && castedByMod) {
+        if (!isListening && message.contains(PAGE_TITLE)) {
             // get the page count
             try {
                 pages = Integer.parseInt(message.substring(message.indexOf("of ") + 3, message.indexOf("of ") + 4));
@@ -103,37 +99,51 @@ public class ServersCommandParser {
                 pages = 3;
             }
 
-            if(castedByMod) {
-                // get the other pages
-                for(int i = 2; i <= pages; i++) {
-                                    Minecraft.getMinecraft().thePlayer.sendChatMessage("/servers " + i);
-                }
-                try {
-                    List chatLines;
-                    // get the lines
-                    chatLines = (List)ModLoader.getPrivateValue(GuiNewChat.class, Minecraft.getMinecraft().ingameGUI.getChatGUI(), 3);
-                    for(int c = 0; c < 20; c++) {
-                        ChatLine line = (ChatLine)chatLines.get(c);
-                        if(StringUtils.stripControlCodes(line.getChatLineString()).equals("--------- Overcast Network Servers (1 of 3) ---------")) {
-                            chatLines.remove(c);
-                            break;
-                        }
+            try {
+                currentPage = Integer.parseInt(message.substring(message.indexOf(" of ") - 1, message.indexOf(" of ")));
+            } catch (Exception e) {
+                currentPage = 1;
+            }
+
+            if (castedByMod) {
+                if(message.contains(FIRST_PAGE_TITLE)) {
+                    // get the other pages
+                    for(int i = 2; i <= pages; i++) {
+                        Minecraft.getMinecraft().thePlayer.sendChatMessage("/servers " + i);
                     }
-                    ModLoader.setPrivateValue(GuiNewChat.class, Minecraft.getMinecraft().ingameGUI.getChatGUI(), 3, chatLines);
-                } catch (Exception e) {
-                    System.out.println("[UndercastMod]: Getting a private value (chatLines) failed");
-                    System.out.println("[UndercastMod]: ERROR: " + e.toString());
                 }
                 isListening = true;
+                removeChatMessage();
             }
         }
     }
 
     public static void castByMod() {
-        if(!isListening) {
+        if (!isListening) {
             castedByMod = true;
         } else {
             nextCastedByMod = true;
+        }
+    }
+
+    private static void removeChatMessage(){
+        try {
+            List chatLines;
+            // get the lines
+            chatLines = (List)ModLoader.getPrivateValue(GuiNewChat.class, Minecraft.getMinecraft().ingameGUI.getChatGUI(), 3);
+            // remove the message (20 most recent chat messages are enough)
+            for(int c = 0; c < 20; c++) {
+                ChatLine line = (ChatLine)chatLines.get(c);
+                if(StringUtils.stripControlCodes(line.getChatLineString()).contains(PAGE_TITLE) || StringUtils.stripControlCodes(line.getChatLineString()).contains(MESSAGE_CHARACTERISTIC)) {
+                    chatLines.remove(c);
+                    break;
+                }
+            }
+            // set them back
+            ModLoader.setPrivateValue(GuiNewChat.class, Minecraft.getMinecraft().ingameGUI.getChatGUI(), 3, chatLines);
+        } catch (Exception e) {
+            System.out.println("[UndercastMod]: Getting a private value (chatLines) failed");
+            System.out.println("[UndercastMod]: ERROR: " + e.toString());
         }
     }
 }
