6a018ebc019d98c64d6cd954fe68a501f1b9fd48
palechip <palechip@gmail.com>
2013-07-03 15:54:08 +0200

    Use the new Start.java

--- Start.java
+++ Start.java
@@ -5,58 +5,90 @@ import java.lang.reflect.Field;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
+import java.util.Arrays;
 
-import net.minecraft.client.Minecraft;
+import joptsimple.ArgumentAcceptingOptionSpec;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+
+import net.minecraft.src.Minecraft;
+import net.minecraft.client.main.Main;
 
 public class Start
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws Exception
     {
-        try
-        {
-            Field f = Minecraft.class.getDeclaredField("minecraftDir");
-            Field.setAccessible(new Field[] { f }, true);
-            f.set(null, new File("."));
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-            return;
-        }
+        int userIndex = -1;
+        int passIndex = -1;
+        int sessIndex = -1;
+        int versIndex = -1;
 
-        if (args.length != 2)
+        for (int x = 0; x < args.length; x++)
         {
-            Minecraft.main(args);
-        }
-        else
-        {
-            try
+            if (args[x].equals("--username"))
             {
-                String parameters = "http://login.minecraft.net/?user=" + URLEncoder.encode(args[0], "UTF-8") +
-                        "&password=" + URLEncoder.encode(args[1], "UTF-8") +
-                        "&version=" + 13;
-                String result = openUrl(parameters);
-
-                if (result == null)
-                {
-                    System.out.println("Can't connect to minecraft.net");
-                    return;
-                }
-
-                if (!result.contains(":"))
-                {
-                    System.out.println("Login Failed: " + result);
-                    return;
-                }
-
-                String[] values = result.split(":");
-                Minecraft.main(new String[] {values[2].trim(), values[3].trim()});
+                userIndex = x + 1;
             }
-            catch (Exception e)
+            else if (args[x].equals("--password"))
             {
-                e.printStackTrace();
+                passIndex = x + 1;
+            }
+            else if (args[x].equals("--session"))
+            {
+                sessIndex = x + 1;
+            }
+            else if (args[x].equals("--version"))
+            {
+                versIndex = x + 1;
             }
         }
+
+        if (userIndex != 0 - 1 && passIndex != -1 && sessIndex == -1)
+        {
+            String[] session = getSession(args[userIndex], args[passIndex]);
+
+            if (session != null)
+            {
+                args[userIndex] = session[0];
+                args = concat(args, new String[] {"--session", session[1]});
+            }
+        }
+
+        if (passIndex != -1)
+        {
+            args[passIndex - 1] = "no_password_for_joo";
+            args[passIndex] = "no_password_for_joo";
+        }
+
+        if (versIndex == -1)
+        {
+            args = concat(args, new String[] { "--version", "fml_mcp" });
+        }
+
+        Main.main(args);
+    }
+
+    private static String[] getSession(String username, String password) throws UnsupportedEncodingException
+    {
+        String parameters = "http://login.minecraft.net/?user=" + URLEncoder.encode(username, "UTF-8") +
+                            "&password=" + URLEncoder.encode(password, "UTF-8") +
+                            "&version=" + 13;
+        String result = openUrl(parameters);
+
+        if (result == null)
+        {
+            System.out.println("Can't connect to minecraft.net");
+            return null;
+        }
+
+        if (!result.contains(":"))
+        {
+            System.out.println("Login Failed: " + result);
+            return null;
+        }
+
+        String[] values = result.split(":");
+        return new String[] { values[2].trim(), values[3].trim() };
     }
 
     private static String openUrl(String addr)
@@ -84,5 +116,12 @@ public class Start
         }
 
         return null;
+    }
+
+    private static <T> T[] concat(T[] first, T[] second)
+    {
+        T[] result = Arrays.copyOf(first, first.length + second.length);
+        System.arraycopy(second, 0, result, first.length, second.length);
+        return result;
     }
 }
